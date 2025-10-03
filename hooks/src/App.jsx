58b39308418e381431e968a8438809efb787d837
kessley <79649917+kessley@users.noreply.github.com>
2025-10-03 09:08:00 -0300

    (03/10) React Hooks: Exececicio: 01

---
+++ hooks/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './App.css'
+
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
+
+import Home from './Home'
+import Exercicio01 from './exercicios/01'
+import Exercicio02 from './exercicios/02'
+
+function App() {
+
+  return (
+    <>
+      <h1>Exercícios React Hooks</h1>
+      <BrowserRouter>
+      
+        <ul>
+          <li> <Link to="/">Página inicial</Link> </li>
+          <li> <Link to="/01">Exercício 01</Link> </li>
+          <li> <Link to="/02">Exercício 02</Link> </li>
+        </ul>
+        
+        <hr />
+
+        <Routes>
+          {/* path="/" significa a raiz do website */}
+          <Route path="/" element={<Home />} />
+          <Route path="/01" element={<Exercicio01 />} />
+          <Route path="/02" element={<Exercicio02 />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ hooks/src/exercicios/02.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+
+function Greeting({initialName = ''}) {
+  // 🐨 inicialize o estado como o valor do localStorage
+  // 💰 window.localStorage.getItem('name') ?? initialName
+  const [name, setName] = React.useState(window.localStorage.getItem('name') ?? initialName)
+
+  // 🐨 Aqui é onde usamos `React.useEffect`.
+  // A função deve armazenar `name` no localStorage.
+  // 💰 window.localStorage.setItem('name', name)
+
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+  }, [name])
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Olá {name}</strong> : 'Por favor, informe seu nome'}
+    </div>
+  )
+}
+
+function Exercicio02() {
+  return <Greeting />
+}
+
+export default Exercicio02
